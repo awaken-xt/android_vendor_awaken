SuperiorOS/android_vendor_support

support: Sync SecureSettingSwitchPreference with AEX

src/com/superior/support/preferences/SecureSettingSwitchPreference.java
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2014 The CyanogenMod project
- * Copyright (C) 2017 AICP
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,33 +17,49 @@
 package com.superior.support.preferences;
 
 import android.content.Context;
+import android.provider.Settings;
 import android.util.AttributeSet;
 
-public class SecureSettingSwitchPreference extends SwitchPreference {
+import androidx.preference.SwitchPreference;
 
+public class SecureSettingSwitchPreference extends SwitchPreference {
     public SecureSettingSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
     }
 
     public SecureSettingSwitchPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
     }
 
     public SecureSettingSwitchPreference(Context context) {
-        super(context);
-        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+        super(context, null);
+    }
+
+    @Override
+    protected boolean persistBoolean(boolean value) {
+        if (shouldPersist()) {
+            if (value == getPersistedBoolean(!value)) {
+                // It's already there, so the same as persisting
+                return true;
+            }
+            Settings.Secure.putInt(getContext().getContentResolver(), getKey(), value ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected boolean getPersistedBoolean(boolean defaultReturnValue) {
+        if (!shouldPersist()) {
+            return defaultReturnValue;
+        }
+        return Settings.Secure.getInt(getContext().getContentResolver(),
+                getKey(), defaultReturnValue ? 1 : 0) != 0;
     }
 
     @Override
     protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
-        // This is what default TwoStatePreference implementation is doing without respecting
-        // real default value:
-        //setChecked(restoreValue ? getPersistedBoolean(mChecked)
-        //        : (Boolean) defaultValue);
-        // Instead, we better do
-        setChecked(restoreValue ? getPersistedBoolean((Boolean) defaultValue)
+        setChecked(Settings.Secure.getString(getContext().getContentResolver(), getKey()) != null ? getPersistedBoolean(isChecked())
                 : (Boolean) defaultValue);
     }
 }

awaken-xt/android_vendor_awaken

support/src/com/awaken/support/preferences/SecureSettingSwitchPreference.java
@@ -1,6 +1,6 @@
 /**
- * Copyright (C) 2014-2016 The CyanogenMod Project
  * Copyright (C) 2018 The LineageOS Project
+/*
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,8 +21,9 @@ import android.content.Context;
 import android.provider.Settings;
 import android.util.AttributeSet;
 
-public class SecureSettingSwitchPreference extends SelfRemovingSwitchPreference {
+import androidx.preference.SwitchPreference;
 
+public class SecureSettingSwitchPreference extends SelfRemovingSwitchPreference {
     public SecureSettingSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
     }
@@ -50,4 +51,32 @@ public class SecureSettingSwitchPreference extends SelfRemovingSwitchPreference 
         return Settings.Secure.getInt(getContext().getContentResolver(),
                 key, defaultValue ? 1 : 0) != 0;
     }
+
+    @Override
+    protected boolean persistBoolean(boolean value) {
+        if (shouldPersist()) {
+            if (value == getPersistedBoolean(!value)) {
+                // It's already there, so the same as persisting
+                return true;
+            }
+            Settings.Secure.putInt(getContext().getContentResolver(), getKey(), value ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected boolean getPersistedBoolean(boolean defaultReturnValue) {
+        if (!shouldPersist()) {
+            return defaultReturnValue;
+        }
+        return Settings.Secure.getInt(getContext().getContentResolver(),
+                getKey(), defaultReturnValue ? 1 : 0) != 0;
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
+        setChecked(Settings.Secure.getString(getContext().getContentResolver(), getKey()) != null ? getPersistedBoolean(isChecked())
+                : (Boolean) defaultValue);
+   }
 }